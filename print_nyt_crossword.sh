2b517e146c6a21e5ddc07f09cf2180c83059a746
Bert Wagner <bertwagner@bertwagner.com>
2021-10-20 08:19:54 -0400

    add print commands

--- print_nyt_crossword.sh
+++ print_nyt_crossword.sh
@@ -3,9 +3,11 @@
 USERNAME=""
 PASSWORD=""
 
-# Remove cookies and previous crossword
+# Remove cookies 
 rm cookies.txt
-rm crossword.pdf
+
+# Create today's date string in NYT expected format
+DATE=$(date +%b%d%y)
 
 # Parse out the CSRF auth token
 AUTH_TOKEN=$(curl -v -c cookies.txt -b cookies.txt "https://myaccount.nytimes.com/auth/enter-email?response_type=cookie&client_id=lgcl&redirect_uri=https%3A%2F%2Fwww.nytimes.com" 2>&1 | grep -oP '(?<=authToken&quot;:&quot;).*?(?=&quot;)')
@@ -19,9 +21,5 @@ curl -c cookies.txt -b cookies.txt -X POST -d "{\"email\":\"${USERNAME}\",\"auth
 # Second page that asks for password
 curl -c cookies.txt -b cookies.txt -X POST -d "{\"username\":\"${USERNAME}\",\"auth_token\":\"${AUTH_TOKEN}\",\"form_view\":\"login\",\"password\":\"${PASSWORD}\",\"remember_me\":\"Y\"}" "https://myaccount.nytimes.com/svc/lire_ui/login" #-H "Content-Type: application/json"
 
-# Download the print editions PDF
-curl -c cookies.txt -b cookies.txt "https://www.nytimes.com/svc/crosswords/v2/puzzle/print/Oct2021.pdf" -o "crossword.pdf"
-
-#TODO: Create today's date string
-
-# TODO: Send to printer
+# Download the print edition of the crossword and send to default printer
+curl -s -b cookies.txt "https://www.nytimes.com/svc/crosswords/v2/puzzle/print/${DATE}.pdf" | lpr -o media=Letter -o fit-to-page
